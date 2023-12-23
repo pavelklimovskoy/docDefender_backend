f1ebb3556d8cc0773d77a86badd3fbd8476ddba9
pavel <pavelklimovskoy@mail.ru>
2023-12-23 19:37:53 +0300

    fix

--- web_application/document_processor/processors/pdf_processor.py
+++ web_application/document_processor/processors/pdf_processor.py
@@ -1,7 +1,7 @@
-# import pytesseract
-# from anonympy.pdf import pdfAnonymizer
-#
-# from docDefender_backend import settings
+import pytesseract
+from anonympy.pdf import pdfAnonymizer
+
+from docDefender_backend import settings
 from docDefender_backend import settings
 from docx import Document
 
@@ -14,8 +14,6 @@ class PDFProcessor:
     def __init__(self, document_name: str):
         self.document_name: str = document_name
         self.output_document_name: str = f'{self.document_name}'
-
-        self.document = Document(settings.MEDIA_ROOT + self.document_name)
 
     def anonymize_doc(self):
         anonym = pdfAnonymizer(path_to_pdf=f'{settings.MEDIA_ROOT}{self.document_name}')
